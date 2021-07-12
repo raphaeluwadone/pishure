a1b50dd99bbab91614b97403bd690a4aa1d0ece3
chinonso <emmanuelchinons9@gmail.com>
2021-07-12 18:20:47 +0100

    change header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,30 +3,34 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = ({ children, ...restProps }) => {
-  return <Nav {...restProps}>{children}</Nav>;
+  return (
+    <Nav {...restProps}>
+      <Container>{children}</Container>
+    </Nav>
+  );
 };
 
 const Nav = styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   position: fixed;
   z-index: 1000;
   top: 0;
-  ${"" /* left: 0; */}
-  max-width: 83rem;
+  left: 0;
   width: 100%;
+  background-color: var(--black-900);
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 83rem;
   margin: 0 auto;
+  width: 100%;
   height: 6.25rem;
   font-size: 0.875rem;
-  background-color: var(--black-900);
 
-  @media (max-width: 1367px) {
-    padding-right: 4rem;
-  }
-
-  @media (max-width: 400px) {
-    padding: 0 1.5rem;
+  @media (max-width: 1364px) {
+    padding: 0 2rem;
   }
 `;
 
---
+++ src/components/Masonry.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Masonry = ({ items }) => {
+  return <div></div>;
+};
+
+export default Masonry;
